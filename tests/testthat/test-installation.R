b4e06ea9d4e183c029f65ecf99038eae6b2938f4
Marc A. Suchard <msuchard@ucla.edu>
2024-11-08 19:29:33 -0800

    fix test

--- tests/testthat/test-installation.R
+++ tests/testthat/test-installation.R
@@ -98,7 +98,7 @@ test_that("Simulate normal random variable", {
   if (supportsJava8()) {
     trace <- simulateNormalDistribution(mean = 1,
                                         sd = 2,
-                                        chainLength = 1000,
+                                        chainLength = 10000,
                                         subSampleFrequency = 1,
                                         seed = 666)
     expect_equal(mean(trace), 1, tolerance = 1E-1)
